289e2abc6bb59f24ac0ec1ee3f7ebcbd1f2ca6e2
Yayo <geramail28@gmail.com>
2021-04-11 22:27:52 +0800

    First commit

---
+++ lib/src/ui/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_simple_firebase_auth/src/bloc/auth_cubit.dart';
+
+class HomeScreen extends StatelessWidget {
+  static Widget create(BuildContext context) => HomeScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: BlocBuilder<AuthCubit, AuthState>(
+        buildWhen: (previous, current) => current is AuthSignedIn,
+        builder: (_, state) {
+          final authUser = (state as AuthSignedIn).user;
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('User: ${authUser.uid}'),
+                SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () => context.read<AuthCubit>().signOut(),
+                  child: Text("Logout"),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
